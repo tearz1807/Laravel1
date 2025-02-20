da836e3f3bb108b607773a267cdf80bbcba900e5
tearz1807 <vlad6_64@list.ru>
2025-02-20 11:19:06 +0300

    меню регестрации и входа

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,9 +1,64 @@
 <template>
-    <div class="d-inline">321</div>App
+    <div id="app">
+            <div class="heading">
+            <h1>Добро пожаловать!</h1>
+        </div>
+    <div class="container">
+        <div class="reg-menu">
+            <div class="menu-top">
+                <a href="#" @click.prevent="showRegister">Регистрация</a>
+                <a href="#" @click.prevent="showLogin">Вход</a>
+            </div>
+            <div v-if="isRegisterVisible" class="sign-up">
+                <form @submit.prevent="handleRegister">
+                    <p><input v-model="email" placeholder="Введите почту" required /></p>
+                    <p><input v-model="registerLogin" placeholder="Введите логин" required /></p>
+                    <p><input type="password" v-model="registerPassword" placeholder="Введите пароль" required /></p>
+                    <p><input type="password" v-model="confirmPassword" placeholder="Повторите пароль" required /></p>
+                    <p><input type="submit" value="Зарегистрироваться" /></p>
+                </form>
+            </div>
+            <div v-if="isLoginVisible" class="sing-in">
+                <form @submit.prevent="handleLogin">
+                    <p><input v-model="login" placeholder="Введите логин" required /></p>
+                    <p><input type="password" v-model="password" placeholder="Введите пароль" required /></p>
+                    <p><input type="submit" value="Войти" /></p>
+                </form>
+            </div>
+        </div>
+    </div>
+    </div>
 </template>
-<script>
-export default {
-    mounted() {
+  
+  <script>
+  export default {
+    data() {
+      return {
+        isLoginVisible: true,
+        isRegisterVisible: false,
+        login: '',
+        password: '',
+        email: '',
+        registerLogin: '',
+        registerPassword: '',
+        confirmPassword: ''
+      };
+    },
+    methods: {
+      showLogin() {
+        this.isLoginVisible = true;
+        this.isRegisterVisible = false;
+      },
+      showRegister() {
+        this.isLoginVisible = false;
+        this.isRegisterVisible = true;
+      },
+      handleLogin() {
+        console.log('Login:', this.login, this.password);
+      },
+      handleRegister() {
+        console.log('Register:', this.email, this.registerLogin, this.registerPassword, this.confirmPassword);
+      }
     }
-}
-</script>
+  };
+  </script>
--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -1,25 +1,5 @@
 import {createStore} from 'vuex'
 
 export default createStore({
-    state: {
-        test: 'test Any Components',
-        version:'',
-    },
-    actions: {
-        testAction(context, payload) {
-            console.log('Action store test',payload)
-        }
-    },
-    getters: {},
-    mutations: {
-        SET_TEST(state, payload) {
-            console.log('mutation store set_test', payload);
-            return state.test = payload;
-        },
-        SET_ASOC(state, payload){
-            for (let key in payload){
-                state[key]=payload[key];
-            }
-        }
-    }
+
 });
