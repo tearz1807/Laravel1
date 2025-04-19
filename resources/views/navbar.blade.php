b56acdefdf0d49fa5558c645ef35a105d1c5ffa0
tearz1807 <vlad6_64@list.ru>
2025-04-20 01:23:00 +0300

    Настроил навбар при уменьшении экрана

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,9 +1,9 @@
-<nav class="navbar navbar-expand-lg w-100">
+<nav class="navbar navbar-expand-xxl w-100">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">
             <i class="bi bi-bank2"></i>
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Переключить навигацию">
+        <button class="navbar-toggler" type="button" data-bs-toggle="modal" data-bs-target="#navbarModal" aria-controls="navbarModal" aria-expanded="false" aria-label="Переключить навигацию">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
@@ -20,6 +20,28 @@
         </div>
     </div>
 </nav>
+
+<div class="modal fade" id="navbarModal" tabindex="-1" aria-labelledby="navbarModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="navbarModalLabel">Навигация</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Закрыть"></button>
+            </div>
+            <div class="modal-body">
+                <ul class="list-group">
+                    <li class="list-group-item"><a href="#">Главная</a></li>
+                    <li class="list-group-item"><a href="#">Напоминания</a></li>
+                    <li class="list-group-item"><a href="#">Карта знаний</a></li>
+                    <li class="list-group-item"><a href="#">Как пользоваться</a></li>
+                </ul>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Закрыть</button>
+            </div>
+        </div>
+    </div>
+</div>
 
 <div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
     <div class="modal-dialog">
